03b7961342065de7dce59a362e604e812a5c1d1d
nahroege <egelila@gmail.com>
2019-04-16 13:59:05 +0300

    proje oluşturuldu

---
+++ agiryasamlar.js
@@ -0,0 +1,4 @@
+var lbs = prompt("Lütfen ağır yaşama katılımcısının ağırlığını lbs cinsinden girin.")
+var kg = lbs*0.454
+alert("Katılımcı  "+kg+" kilogram ağırlığındadır.")
+console.log("Conversion Completed")
